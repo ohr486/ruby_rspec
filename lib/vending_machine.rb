e6abc33ea374703298fd45a82f1da496802688fa
ohara_tsunenori <tsunenori.ohara@drecom.co.jp>
2013-07-27 16:19:35 +0900

    add counting coins utils

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -1,14 +1,27 @@
 # -*- encoding: utf-8 -*-
 
 class VendingMachine
-  attr_accessor :amount
+  attr_accessor :amount, 
+    :count_of_y10,  :count_of_y50,  :count_of_y100,
+    :count_of_y500, :count_of_y1000
 
   def initialize
     @amount = 0
+    @count_of_y10   = 0
+    @count_of_y50   = 0
+    @count_of_y100  = 0
+    @count_of_y500  = 0
+    @count_of_y1000 = 0
   end
 
-  def insert(money)
+  def stock_money(money)
     @amount += money
+  end
+
+  [10, 50, 100, 500, 1000].each do |money|
+    define_method("insert#{money}") do
+      stock_money(money)
+    end
   end
 
   def refund
--- spec/lib/vending_machine_spec.rb
+++ spec/lib/vending_machine_spec.rb
@@ -14,20 +14,20 @@ describe VendingMachine do
     # ... pending ...
   end
 
-  describe "#insert" do
-    it "10円をinsertする" do
-      vending_machine.insert(10)
+  describe "#stock_money" do
+    it "10円をstock_moneyする" do
+      vending_machine.stock_money(10)
       vending_machine.amount.should == 10
     end
 
-    it "50円をinsertする" do
-      vending_machine.insert(50)
+    it "50円をstock_moneyする" do
+      vending_machine.stock_money(50)
       vending_machine.amount.should == 50
     end
 
-    it "10円と50円をinsertする" do
-      vending_machine.insert(10)
-      vending_machine.insert(50)
+    it "10円と50円をstock_moneyする" do
+      vending_machine.stock_money(10)
+      vending_machine.stock_money(50)
       vending_machine.amount.should == 60
     end
   end
@@ -38,15 +38,48 @@ describe VendingMachine do
     end
     
     it "10円の状態でrefundすると10円が返却される" do
-      vending_machine.insert(10)
+      vending_machine.stock_money(10)
       vending_machine.refund.should == 10
     end
 
     it "10円の状態でrefundするとamountが0円になる" do
-      vending_machine.insert(10)
+      vending_machine.stock_money(10)
       vending_machine.refund
       vending_machine.amount.should == 0
     end
   end
+
+  describe "#stock_moneyN" do
+    it "stock_moneyNでstock_money(N)がよばれる" do
+      [10,50,100,500,1000].each do |money|
+        vending_machine.should_receive(:stock_money).with(money)
+        vending_machine.send("insert#{money}")
+      end
+    end
+  end
+
+  describe "#count_of_y10" do
+    it "初期状態ではcount_of_y10は0" do
+      vending_machine.count_of_y10.should == 0
+    end
+  end
+  
+  describe "#count_of_yN" do
+    it "初期状態ではcount_of_yNは0" do
+      [10,50,100,500,1000].each do |money|
+        vending_machine.send("count_of_y#{money}").should == 0
+      end
+    end
+
+    it "count_of_yNで投入された回数が取得される" do
+      pending
+      [10,50,100,500,1000].each do |money|
+        vending_machine.send("insert#{money}")
+        vending_machine.send("count_of_y#{money}").should == 1
+      end
+    end
+  end
+
+
 
 end
